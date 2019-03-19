b8afe68e148b6e6a56727d3407cffd811c67f542
vincen <vincen.gao@gmail.com>
2019-03-19 17:26:15 +0800

    add token

--- Nroad.py
+++ Nroad.py
@@ -3,10 +3,11 @@
 
 __author__ = 'vincen'
 
-from flask import Flask, redirect, session, url_for, escape, request, jsonify
+from flask import Flask, redirect, url_for, escape, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from crypt import crypt
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 ########################################################################
 #                           global varialbe
@@ -15,13 +16,27 @@ app = Flask(__name__)
 # enable debug mode
 app.debug = True
 # flask session needs a secret key
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+# a secret key using by token
+app.config['SECRET_KEY'] = 'nroad is a good company'
 # database configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://cc3:cc3@localhost/cc3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # jsonify serialize chinese word, not original unicode
 app.config["JSON_AS_ASCII"] = False
 db = SQLAlchemy(app)
+salt = 'wangxing'
+
+SCHOOL = {
+        '10704': '西安科技大学',
+        '12712': '西安欧亚学院',
+        '11664': '西安邮电大学',
+        '10716': '陕西中医药大学',
+        '10722': '咸阳师范学院',
+        '10698': '西安交通大学',
+        '10697': '西北大学',
+        '0820' : 0
+    }
 
 
 ########################################################################
@@ -56,8 +71,32 @@ def dashboard():
     temp = order_service.get_orders()
     return jsonify({'tasks': temp}), 200
 
+@app.route("/v1/users", methods=['POST'])
+def create_user():
+    username = request.json.get('username')
+    nickname = request.json.get('nickname')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)      # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)      # existing user
+    user = User(username = username, nickname = nickname)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('read_user', pkid=user.pkid, _external=True)})
+
+@app.route("/v1/users/<int:pkid>", methods=['GET'])
+def read_user(pkid):
+    user = User.query.get(pkid)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username, 'nickname': user.nickname})
+
+
 @app.route("/v1/yesterday/new", methods=['GET'])
-def new_orders():
+def new_orders_yesterday_api1():
     """
     new orders in yesterday
     """
@@ -66,7 +105,7 @@ def new_orders():
     return jsonify({'result': temp[0]}), 200
 
 @app.route("/v1/yesterday/top1", methods=['GET'])
-def top1_orders():
+def top1_orders_yesterday_api1():
     """
     top 1 in yesterday
     """
@@ -76,10 +115,21 @@ def top1_orders():
     return jsonify({'result': result}), 200
 
 @app.route("/v1/total/all", methods=['GET'])
-def all_orders():
+def all_orders_count_api1():
     order_service = OrderService()
     temp = order_service.get_total_orders_count()
+    return jsonify({'result': temp[0]}), 200
 
+@app.route("/v1/total/top1", methods=['GET'])
+def top1_orders_count_api1():
+    order_service = OrderService()
+    temp = order_service.get_top1_orders_count()
+    result = {'school': temp[0], 'count': temp[1]}
+    return jsonify({'result': result}), 200
+
+@app.route("/v1/daily/detail", methods=['GET'])
+def daily_detail_count_api1():
+    psss
 
 ########################################################################
 #                               service
@@ -120,6 +170,29 @@ class OrderService(object):
                 );
         '''
         return db.session.execute(sql, {"order_time": self.yesterday}).fetchone()
+
+    def get_total_orders_count(self):
+        return db.session.query(db.func.sum(Order.count)).one()
+    
+    def get_top1_orders_count(self):
+        sql = '''
+            SELECT t.school, SUM ( t.count ) 
+            FROM order_generalize t 
+            GROUP BY t.school 
+            HAVING
+                SUM ( t.count ) = (
+                SELECT MAX( r.topcount ) 
+                FROM ( 
+                    SELECT P.school sch, SUM ( P.count ) topcount 
+                    FROM order_generalize P 
+                    GROUP BY P.school ) r 
+                );
+        '''
+        return db.session.execute(sql).fetchone()
+
+    def get_daily_detail_count(self):
+        pass
+
 
 ########################################################################
 #                           dao
@@ -173,36 +246,45 @@ class Product(db.Model):
         return dict
 
 
+class User(db.Model):
+    __tablename__ = 'user'
+
+    pkid = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    nickname = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def hash_password(self, password):
+        self.password = crypt(password, salt=salt)
+
+    def verify_password(self, password):
+        p2 = crypt(password, salt=salt)
+        if p2 == self.password:
+            return True
+        else:
+            return False
+    
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({'pkid': self.pkid})
+    
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None     # token expired
+        except BadSignature:
+            return None     # invalid token
+        user = User.query.get(data['pkid'])
+        return user
+
+    
 
 ########################################################################
 #                                   utils
 ########################################################################
-# def new_alchemy_encoder():
-#     _visited_objs = []
-
-#     class AlchemyEncoder(json.JSONEncoder):
-#         def default(self, obj):
-#             if isinstance(obj.__class__, DeclarativeMeta):
-#                 # don't re-visit self
-#                 if obj in _visited_objs:
-#                     return None
-#                 _visited_objs.append(obj)
-
-#                 # an SQLAlchemy class
-#                 fields = {}
-#                 for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-#                     data = obj.__getattribute__(field)
-#                     try:
-#                         if isinstance(data, datetime):
-#                             data = data.strftime('%Y-%m-%d %H:%M:%S')
-#                         json.dumps(data)  # this will fail on non-encodable values, like other classes
-#                         fields[field] = data
-#                     except TypeError:
-#                         fields[field] = None
-#                 return fields
-
-#             return json.JSONEncoder.default(self, obj)
-#     return AlchemyEncoder
 class Utils(object):
     def get_yesterday(self):
         """
@@ -210,6 +292,7 @@ class Utils(object):
         """
         yesterday = datetime.date.today() - datetime.timedelta(days=1)
         return yesterday
+    
 
 
 
