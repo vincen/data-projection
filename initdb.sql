48204c85481aa964124d5f453414f999a9cadb52
vincen <vincen.gao@gmail.com>
2019-03-21 23:21:06 +0800

    update sql

--- initdb.sql
+++ initdb.sql
@@ -1,8 +1,8 @@
 -- ----------------------------
--- Table structure for order_generalize
+-- Table structure for t_order
 -- ----------------------------
-DROP TABLE IF EXISTS "public"."order_generalize";
-CREATE TABLE "public"."order_generalize" (
+DROP TABLE IF EXISTS "public"."t_order";
+CREATE TABLE "public"."t_order" (
   "pkid" serial NOT NULL,
   "pid" varchar(255),
   "product_name" varchar(255),
@@ -12,64 +12,85 @@ CREATE TABLE "public"."order_generalize" (
   "updated_at" timestamp(6) without time zone NOT NULL DEFAULT now(),
   "order_time" date
 );
-ALTER TABLE "public"."order_generalize" OWNER TO "cc3";
+ALTER TABLE "public"."t_order" OWNER TO "cc3";
 
 -- ----------------------------
--- Primary Key structure for table order_generalize
+-- Primary Key structure for table t_order
 -- ----------------------------
-ALTER TABLE "public"."order_generalize" ADD CONSTRAINT "order_generalize_pkey" PRIMARY KEY ("pkid");
+ALTER TABLE "public"."t_order" ADD CONSTRAINT "t_order_pkey" PRIMARY KEY ("pkid");
 
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for t_user
 -- ----------------------------
-DROP TABLE IF EXISTS "public"."user";
-CREATE TABLE "public"."user" (
+DROP TABLE IF EXISTS "public"."t_user";
+CREATE TABLE "public"."t_user" (
   "pkid" serial NOT NULL,
   "username" varchar(255),
   "nickname" varchar(255),
   "password" varchar(255),
   "created_at" timestamp(6) with time zone NOT NULL DEFAULT now()
 );
-ALTER TABLE "public"."user" OWNER TO "cc3";
+ALTER TABLE "public"."t_user" OWNER TO "cc3";
 -- ----------------------------
--- Primary Key structure for table user
+-- Primary Key structure for table t_user
 -- ----------------------------
-ALTER TABLE "public"."user" ADD CONSTRAINT "user_pkey" PRIMARY KEY ("pkid");
+ALTER TABLE "public"."t_user" ADD CONSTRAINT "t_user_pkey" PRIMARY KEY ("pkid");
 
 
 -- ----------------------------
--- Table structure for product
+-- Table structure for t_product
 -- ----------------------------
-DROP TABLE IF EXISTS "public"."product";
-CREATE TABLE "public"."product" (
+DROP TABLE IF EXISTS "public"."t_product";
+CREATE TABLE "public"."t_product" (
   "pkid" serial NOT NULL,
   "pid" varchar(255),
   "product_name" varchar(255),
   "school" varchar(255),
   "carrier" varchar(255),
   "is_boss" bool,
-  "precentage" numeric(8,2)
+  "percentage" numeric(8,2)
 );
-ALTER TABLE "public"."product" OWNER TO "cc3";
+ALTER TABLE "public"."t_product" OWNER TO "cc3";
 -- ----------------------------
--- Primary Key structure for table product
+-- Primary Key structure for table t_product
 -- ----------------------------
-ALTER TABLE "public"."product" ADD CONSTRAINT "product_pkey" PRIMARY KEY ("pkid");
+ALTER TABLE "public"."t_product" ADD CONSTRAINT "t_product_pkey" PRIMARY KEY ("pkid");
 
 
 -- ----------------------------
--- Table structure for permission
+-- Table structure for t_permission
 -- ----------------------------
-DROP TABLE IF EXISTS "public"."permission";
-CREATE TABLE "public"."permission" (
+DROP TABLE IF EXISTS "public"."t_permission";
+CREATE TABLE "public"."t_permission" (
   "pkid" serial NOT NULL,
   "user_id" int4,
   "school_code" varchar(255),
   "carrier" varchar(255)
 );
-ALTER TABLE "public"."permission" OWNER TO "cc3";
+ALTER TABLE "public"."t_permission" OWNER TO "cc3";
 -- ----------------------------
--- Primary Key structure for table permission
+-- Primary Key structure for table t_permission
 -- ----------------------------
-ALTER TABLE "public"."permission" ADD CONSTRAINT "permission_pkey" PRIMARY KEY ("pkid");
+ALTER TABLE "public"."t_permission" ADD CONSTRAINT "t_permission_pkey" PRIMARY KEY ("pkid");
+
+
+-- ----------------------------
+-- View structure for v_order
+-- ----------------------------
+DROP VIEW IF EXISTS "public"."v_order";
+CREATE VIEW "public"."v_order" AS
+  SELECT
+    o.order_time,
+    o.school,
+    r.carrier,
+    CAST(SUM ( COUNT * CASE WHEN is_boss THEN r.percentage ELSE 1 END ) AS INT) AS orders 
+  FROM
+    t_order o,
+    t_product r 
+  WHERE
+    o.pid = r.pid 
+  GROUP BY
+    o.order_time,
+    o.school,
+    r.carrier;
